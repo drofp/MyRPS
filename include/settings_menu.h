9f8a6b5b08ff0107e92c8c8c4c2f7536a52f74f7
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2019-02-28 18:57:16 -0800

    Add Ability to Set Difficulty (#19)
    * Add files via upload
    
    Setting menu and option header
    
    * Add files via upload
    
    Setting menu
    
    * syntax change
    
    * syntax change
    
    * bug fixed
    
    * Delete SettingsMenu.cpp
    
    * bug fix

---
+++ include/settings_menu.h
@@ -0,0 +1,25 @@
+#ifndef SETTINGS_MENU_H_
+#define SETTINGS_MENU_H_
+
+#include <vector>
+
+#include "setting_option.h"
+
+using namespace std;
+using namespace SettingOptions;
+
+namespace myrps
+{
+    class SettingsMenu
+    {
+    public:
+        SettingsMenu();
+        void displaySettings();
+        
+    private:
+        vector<SettingOption> settings;
+        SettingOption GetUserChoice();
+        SettingOption ValidateSetting(string val);
+    };
+}
+#endif
---
+++ src/settings_menu.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <boost/algorithm/string.hpp>
+
+#include "settings_menu.h"
+#include "setting_option.h"
+
+using namespace std;
+using namespace SettingOptions;
+
+namespace myrps
+{
+    SettingsMenu::SettingsMenu()
+    {
+        for (int i = 0; i < num_options; i++)
+        {
+            settings.push_back(options_list[i]);
+        }
+    }
+
+    void SettingsMenu::displaySettings()
+    {
+        cout << "Select difficulty!" << endl;
+        SettingOption user_choice = GetUserChoice();
+        switch (user_choice)
+        {
+            case SettingOption::D_EASY:
+                std::cout << "Easy";
+                break;
+            case SettingOption::D_MEDIUM:
+                std::cout << "Medium";
+                break;
+            case SettingOption::D_HARD:
+                std::cout << "Hard";
+                break;
+
+            default:
+                std::cout << "Whatever";
+                break;
+        }
+    }
+
+    SettingOption SettingsMenu::GetUserChoice()
+    {
+        bool notValid = true;
+        SettingOption user_choice;
+        while(notValid)
+        {
+            string user_input;
+            cout << "Here are your options:" << endl;
+            for(unsigned int i = 0; i < settings.size(); i++)
+            {
+                cout << settings[i] << "(" << i << ")" << endl;
+            }
+            cout << "What would you like to do?: ";
+            cin >> user_input;
+            user_choice = ValidateSetting(user_input);
+            notValid = (user_choice == SettingOption::D_ERROR);
+        }
+        return user_choice;
+    }
+
+
+    SettingOption SettingsMenu::ValidateSetting(string val)
+    {
+        SettingOption user_choice;
+        if (stoi(val) == 0)
+        {
+            user_choice = SettingOption::D_EASY;
+        }
+        else if (stoi(val) == 1)
+        {
+            user_choice = SettingOption::D_MEDIUM;
+        }
+        else if (stoi(val) == 2)
+        {
+            user_choice = SettingOption::D_HARD;
+        }
+        else
+        {
+            user_choice = SettingOption::D_ERROR;
+        }
+        return user_choice;
+    }
+
+} // namespace myrps
