014338200c9a6842351204e0b4b4a9e054f444d4
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2019-03-07 21:10:04 -0800

    Start ML implementation

--- include/ml_chooser.h
+++ include/ml_chooser.h
@@ -1,14 +1,36 @@
 #ifndef ML_CHOOSER_H
 #define ML_CHOOSER_H
 
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <string>
+
 #include "chooser.h"
+
+using namespace std;
 
 namespace myrps
 {
 class MLChooser : public myrps::Chooser
 {
 public:
+  MLChooser();
+  MLChooser(int n);
   Move DecideMove();
+private:
+  unordered_map<string, int> hist_data;
+
+  bool PopulateHistogram();
+  Move GetMostLikelyMove(string last_n_moves);
+  bool CompareMoveFreq(unordered_map<string, int> &a,
+    unordered_map<string, int> &b);
+  vector<pair<string, int>>
+    GetPossibleChoices(string last_n_moves);
+  void
+    InsertPossibleChoice(vector<pair<string, int>> &possible_choices,
+                          string permutation);
 };
 }
 
--- src/ml_chooser.cpp
+++ src/ml_chooser.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "ml_chooser.h"
 
 using namespace std;
@@ -11,5 +9,65 @@ namespace myrps
     Move computer_move = Move::kScissors;
 
     return computer_move;
+  }
+
+  Move MLChooser::GetMostLikelyMove(string last_n_moves)
+  {
+    Move most_likely_move;
+
+    vector<pair<string, int>> possible_moves = 
+      MLChooser::GetPossibleChoices(last_n_moves);
+
+      // sort(possible_moves.begin(), possible_moves.end(), 
+      //   this->CompareMoveFreq);
+
+    return most_likely_move;
+  }
+
+  // bool MLChooser::CompareMoveFreq(unordered_map<string, int> &a,
+  //   unordered_map<string, int> &b)
+  //   {
+
+  //     return a. > b->first;
+  //   }
+
+  // TODO: Replace with search algorithm for expandability. Maybe store hist as 
+  // tree for improved search time.
+  vector<pair<string, int>> 
+  MLChooser::GetPossibleChoices(string last_n_moves)
+  {
+    vector<pair<string, int>> possible_choices;
+
+    string permutation;
+    permutation = last_n_moves + "r";
+    MLChooser::InsertPossibleChoice(possible_choices, permutation);
+
+    permutation = last_n_moves + "p";
+    MLChooser::InsertPossibleChoice(possible_choices, permutation);
+
+    permutation = last_n_moves + "s";
+    MLChooser::InsertPossibleChoice(possible_choices, permutation);
+      
+    return possible_choices;
+  }
+
+  void MLChooser::InsertPossibleChoice(
+    vector<pair<string, int>> &possible_choices,
+    string permutation)
+  {
+    unordered_map<string, int> possible_choice;
+
+    unordered_map<string, int>::iterator it = hist_data.find(permutation);
+    if (it != hist_data.end())
+    {
+      // possible_choice[permutation] = hist_data.find(permutation)->second;
+      possible_choices.push_back(
+        make_pair(permutation, hist_data.find(permutation)->second));
+    }
+    else
+    {
+      possible_choices.push_back(
+        make_pair(permutation, 0));
+    }
   }
 }
