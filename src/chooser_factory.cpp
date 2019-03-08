95ce12e95c2ad168b782af67997c7eb3b6ae3fae
Evan Ugarte <evanuxd@gmail.com>
2019-03-07 19:58:02 -0800

    Integrated Factory to Computer Player
    Now the computer player will change related to user choice

--- src/chooser_factory.cpp
+++ src/chooser_factory.cpp
@@ -1,28 +1,31 @@
 #include "chooser_factory.h"
+#include "setting_option.h"
+#include "random_chooser.h"
+#include "ml_chooser.h"
 
 #include <iostream>
 
 using namespace std;
 using namespace myrps;
 
-Chooser *ChooserFactory::makeChooser(string choice)
+Chooser *ChooserFactory::MakeChooser(SettingOption choice)
 {
-    if(choice == "smart")
-    {
-        cout << "you chose random!" << endl;
-    }
-    else if(choice == "smart")
-    {
-        cout << "you chose smart!" << endl;
-    }
-    else if(choice == "genius")
-    {
-        cout << "you chose genius!" << endl;
-    }
-    else
-    {
-        cout << "you chose none of those!" << endl;
-    }
+  if(choice == SettingOption::D_RANDOM)
+  {
+    return new RandomChooser();
+  }
+  else if(choice == SettingOption::D_SMART)
+  {
+    return new MLChooser();
+  }
+  else if(choice == SettingOption::D_GENIUS)
+  {
+    return new MLChooser();
+  }
+  else
+  {
+    cout << "you chose none of those!" << endl;
+  }
 
-    return nullptr;
+  return nullptr;
 }
